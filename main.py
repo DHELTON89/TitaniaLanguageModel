44afe657bfb956a594f2aad06223b78f86e51f58
DHELTON89 <dannyhelton@outlook.com>
2026-02-24 18:54:01 -0800

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
+"""
     print("Titania Initialization")
     print("Why isn't git working?")
 if __name__ == "__main__":
+"""
+#An embedding is essentially a mapping of discrete objects to
+#points in a continuous vector space, basically they convert 
+#nonnumeric data into a format that neural networks can
+#process. Words, sentences, and even paragraphs are tokenized and 
+#given a value that corresponds to a specific number.
+
     main()
     
