67b25214b5bd36557be4ca317a1b58429b318f74
DHELTON89 <dannyhelton@outlook.com>
2026-02-25 17:48:11 -0800

    added the verdict

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import urllib.request
+url = ("https://raw.githubusercontent.com/rasbt/"
+"LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+"the-verdict.txt")
+file_path = "the-verdict.txt"
+urllib.request.urlretrieve(url, file_path)
+
+
 def main():
-"""
+    """
     print("Titania Initialization")
     print("Why isn't git working?")
 if __name__ == "__main__":
@@ -10,5 +18,13 @@ if __name__ == "__main__":
 #process. Words, sentences, and even paragraphs are tokenized and 
 #given a value that corresponds to a specific number.
 
+#Here we download text from the verdict
+
+    with open("the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    print("Total number of character:", len(raw_text))
+    print(raw_text[:99])
+
+if __name__ == "__main__":
     main()
     
